4e64db24098bceb0f0581c60cb0f69cc81b42e4c
tedtuan123 <55624972+tedtuan123@users.noreply.github.com>
2019-10-22 15:43:27 +0700

    Create EchoClient.java

---
+++ Client/Main/EchoClient.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import com.example.SerialTest;
+import java.io.InputStream;
+import java.io.DataOutputStream; 
+import java.math.BigInteger;
+import java.io.ByteArrayInputStream;
+import com.example.SerialTest;
+public class EchoClient extends SerialTest{
+  
+    public static void main(String[] args) throws IOException {
+    if (args.length != 2) {
+      System.err.println("Usage: java EchoClient <host name> <port number>");
+      System.exit(1);
+    }
+    String hostName = args[0];
+    int portNumber = Integer.parseInt(args[1]);
+     SerialTest main = new SerialTest();
+      main.initialize();
+    try (Socket echoSocket = new Socket(hostName, portNumber);
+            
+      PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+      BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()))){
+      String inputLine;
+	  while ((inputLine = in.readLine()) != null) {
+            System.out.println("Received message: " + inputLine );
+            byte[] data=main.hexStringToByteArray(inputLine);
+            main.sendSerial(data);
+	    out.println(data);            
+	  }
+    }catch (UnknownHostException e) {
+       System.err.println("Don't know about host " + hostName);
+       System.exit(1);
+  
+    } 
+  }
+}
